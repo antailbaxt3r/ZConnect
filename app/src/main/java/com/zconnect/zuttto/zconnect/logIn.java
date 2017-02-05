9245343394fcfcebb800fd9b2b4218a0466f581e
G-LOKi <lokesh96@gamil.com>
2017-02-05 10:53:11 +0530

    Flash Screen and Logo added

---
+++ app/src/main/java/com/zconnect/zuttto/zconnect/logIn.java
@@ -0,0 +1,13 @@
+package com.zconnect.zuttto.zconnect;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class logIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
