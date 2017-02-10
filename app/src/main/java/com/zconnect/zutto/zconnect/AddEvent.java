be7ad7097677ca942563fe657f0e63708fb877da
Shubham Kumar <shubhamkmr1410@gmail.com>
2017-02-10 21:48:43 +0530

    Events UI Improved

--- app/src/main/java/com/zconnect/zutto/zconnect/AddEvent.java
+++ app/src/main/java/com/zconnect/zutto/zconnect/AddEvent.java
@@ -1,20 +1,31 @@
 package com.zconnect.zutto.zconnect;
 
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.net.ConnectivityManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
+import android.view.WindowManager;
+import android.widget.EditText;
 import android.widget.FrameLayout;
-import android.widget.ImageButton;
-import android.widget.Toast;
+import android.widget.TextView;
 
+import com.facebook.drawee.view.SimpleDraweeView;
 import com.github.jjobes.slidedatetimepicker.SlideDateTimeListener;
 import com.github.jjobes.slidedatetimepicker.SlideDateTimePicker;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -31,15 +42,14 @@ import java.io.IOException;
 import java.util.Date;
 
 public class AddEvent extends AppCompatActivity {
-
     private static final int GALLERY_REQUEST = 7;
     String eventDate;
     String dateString;
     private Uri mImageUri = null;
-    private ImageButton mAddImage;
-    private android.support.design.widget.TextInputEditText mEventName;
-    private android.support.design.widget.TextInputEditText mEventDescription;
-    private Button mPostBtn;
+    private SimpleDraweeView mAddImage;
+    private EditText mEventName;
+    private EditText mEventDescription;
+    // private Button mPostBtn;
     private StorageReference mStorage;
     private DatabaseReference mDatabase;
     private FrameLayout CalendarButton;
@@ -48,22 +58,41 @@ public class AddEvent extends AppCompatActivity {
         @Override
         public void onDateTimeSet(Date date) {
             eventDate = date.toString();
-            Toast.makeText(AddEvent.this, eventDate, Toast.LENGTH_SHORT).show();
+            // Toast.makeText(AddEvent.this, eventDate, Toast.LENGTH_SHORT).show();
             dateString = String.valueOf(date.getTime());
-            Toast.makeText(AddEvent.this, dateString, Toast.LENGTH_SHORT).show();
+            //Toast.makeText(AddEvent.this, dateString, Toast.LENGTH_SHORT).show();
 
         }
     };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_event);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if (toolbar != null) {
+            toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    onBackPressed();
+                }
+            });
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
 
-        mAddImage = (ImageButton) findViewById(R.id.imageButton);
-        mEventName = (TextInputEditText) findViewById(R.id.name);
-        mEventDescription = (TextInputEditText) findViewById(R.id.description);
-        mPostBtn = (Button) findViewById(R.id.post);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            int colorPrimary = ContextCompat.getColor(this, R.color.colorPrimary);
+            getWindow().setStatusBarColor(colorPrimary);
+            getWindow().setNavigationBarColor(colorPrimary);
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        }
+        mAddImage = (SimpleDraweeView) findViewById(R.id.imageButton);
+        mEventName = (EditText) findViewById(R.id.name);
+        mEventDescription = (EditText) findViewById(R.id.description);
+        //  mPostBtn = (Button) findViewById(R.id.post);
         mStorage = FirebaseStorage.getInstance().getReference();
         mDatabase = FirebaseDatabase.getInstance().getReference().child("ZConnect/Event/Posts");
 
@@ -74,6 +103,16 @@ public class AddEvent extends AppCompatActivity {
         mAddImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if (ContextCompat.checkSelfPermission(
+                        AddEvent.this,
+                        android.Manifest.permission.WRITE_EXTERNAL_STORAGE) !=
+                        PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(
+                            AddEvent.this,
+                            new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                            12345
+                    );
+                }
                 Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
                 galleryIntent.setType("image/*");
                 startActivityForResult(galleryIntent, GALLERY_REQUEST);
@@ -91,16 +130,38 @@ public class AddEvent extends AppCompatActivity {
             }
         });
 
-        mPostBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                startPosting();
-
-            }
-        });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_add_event, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_done) {
+            if (!isNetworkAvailable(getApplicationContext())) {
+
+                Snackbar snack = Snackbar.make(mEventDescription, "No Internet. Can't Add Event.", Snackbar.LENGTH_LONG);
+                TextView snackBarText = (TextView) snack.getView().findViewById(android.support.design.R.id.snackbar_text);
+                snackBarText.setTextColor(Color.WHITE);
+                snack.getView().setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.teal800));
+                snack.show();
+
+            } else {
+                startPosting();
+            }
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public boolean isNetworkAvailable(final Context context) {
+        final ConnectivityManager connectivityManager = ((ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE));
+        return connectivityManager.getActiveNetworkInfo() != null && connectivityManager.getActiveNetworkInfo().isConnected();
+    }
     private void startPosting() {
         mProgress.setMessage("Posting Event..");
         mProgress.show();
@@ -148,17 +209,24 @@ public class AddEvent extends AppCompatActivity {
             Uri imageUri = data.getData();
             CropImage.activity(imageUri)
                     .setGuidelines(CropImageView.Guidelines.ON)
+                    .setSnapRadius(2)
                     .start(this);
         }
         if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
             CropImage.ActivityResult result = CropImage.getActivityResult(data);
             if (resultCode == RESULT_OK) {
 
+//
+//                mAddImage.setImageURI(mImageUri);
+
                 try {
-                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), mImageUri);
+                    mImageUri = result.getUri();
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(getApplicationContext().getContentResolver(), mImageUri);
                     ByteArrayOutputStream out = new ByteArrayOutputStream();
-                    bitmap.compress(Bitmap.CompressFormat.JPEG, 50, out);
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 30, out);
+
                     String path = MediaStore.Images.Media.insertImage(AddEvent.this.getContentResolver(), bitmap, mImageUri.getLastPathSegment(), null);
+
                     mImageUri = Uri.parse(path);
                     mAddImage.setImageURI(mImageUri);
 
@@ -167,13 +235,13 @@ public class AddEvent extends AppCompatActivity {
                     e.printStackTrace();
                 }
 
-
-
-
-
             } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
                 Exception error = result.getError();
             }
         }
+
     }
 }
+
+
+
--- app/src/main/java/com/zconnect/zutto/zconnect/AllEvents.java
+++ app/src/main/java/com/zconnect/zutto/zconnect/AllEvents.java
@@ -218,7 +218,7 @@ public class AllEvents extends AppCompatActivity {
                 viewHolder.setEventDesc(model.getEventDescription());
                 viewHolder.setEventImage(getApplicationContext(), model.getEventImage());
                 viewHolder.setEventDate(model.getEventDate());
-                viewHolder.openEvent(model.getKey());
+                // viewHolder.openEvent(model.getKey());
                 viewHolder.setEventReminder(model.getEventDescription(), model.getEventName(), model.getFormatDate());
 //                key=model.getKey();
 
