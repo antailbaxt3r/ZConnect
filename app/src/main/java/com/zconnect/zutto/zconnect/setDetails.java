291f5955af1735d041c9835bb871c836bbcc1922
G-LOKi <lokesh96@gamil.com>
2017-04-14 15:53:49 +0530

    select image compress

--- app/src/main/java/com/zconnect/zutto/zconnect/setDetails.java
+++ app/src/main/java/com/zconnect/zutto/zconnect/setDetails.java
@@ -2,14 +2,15 @@ package com.zconnect.zutto.zconnect;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -26,6 +27,9 @@ import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 public class setDetails extends AppCompatActivity {
 
@@ -158,28 +162,42 @@ public class setDetails extends AppCompatActivity {
     }
 
 
-    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == GALLERY_REQUEST && resultCode == RESULT_OK){
+        if (requestCode == GALLERY_REQUEST && resultCode == RESULT_OK) {
             Uri imageUri = data.getData();
             CropImage.activity(imageUri)
                     .setGuidelines(CropImageView.Guidelines.ON)
-                    .setAspectRatio(1,1)
+                    .setAspectRatio(1, 1)
+                    .setSnapRadius(2)
                     .start(this);
         }
         if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
             CropImage.ActivityResult result = CropImage.getActivityResult(data);
             if (resultCode == RESULT_OK) {
 
-                mImageUri= result.getUri();
-                userProfile.setImageURI(mImageUri);
+
+                try {
+                    mImageUri = result.getUri();
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(getApplicationContext().getContentResolver(), mImageUri);
+                    ByteArrayOutputStream out = new ByteArrayOutputStream();
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 10, out);
+                    Bitmap bitmap2 = Bitmap.createScaledBitmap(bitmap, 150, 150, true);
+                    String path = MediaStore.Images.Media.insertImage(setDetails.this.getContentResolver(), bitmap2, mImageUri.getLastPathSegment(), null);
+
+                    mImageUri = Uri.parse(path);
+                    userProfile.setImageURI(mImageUri);
+
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 
             } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
                 Exception error = result.getError();
-                Log.e("Gallery Intent error", error.getMessage());
             }
         }
+
     }
 }
