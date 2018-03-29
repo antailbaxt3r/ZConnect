f7ac4cf10c24ecfa407280930a825bce854695b5
G-LOKi <garg.lokesh96@gmail.com>
2018-03-29 16:17:06 +0530

    Infone recents added completely

--- app/src/main/java/com/zconnect/zutto/zconnect/InfoneAddContactActivity.java
+++ app/src/main/java/com/zconnect/zutto/zconnect/InfoneAddContactActivity.java
@@ -103,22 +103,28 @@ public class InfoneAddContactActivity extends AppCompatActivity {
 
                     Log.e(TAG, "data phone:" + key + " " + phoneNum1);
 
+                    //Inside Categories
                     newContactRef.child("name").setValue(name);
                     newContactRef.child("phone").child("0").setValue(phoneNum1);
                     newContactRef.child("phone").child("1").setValue(phoneNum2);
+                    newContactRef.child("type").child("NotUser");
                     newContactRef.child("key").child(key);
+
+                    //Inside Contacts
                     newContactNumRef.child("category").setValue(catId);
+                    newContactNumRef.child("key").child(key);
                     newContactNumRef.child("name").setValue(name);
                     newContactNumRef.child("phone").child("0").setValue(phoneNum1);
                     newContactNumRef.child("phone").child("1").setValue(phoneNum2);
                     newContactNumRef.child("PostTimeMillis").setValue(postTimeMillis);
+                    newContactNumRef.child("UID").setValue(FirebaseAuth.getInstance().getCurrentUser().getUid());
                     uploadImage();
 
-                    //for Recents
+                    //Inside Recents
                     final DatabaseReference recentsPost = databaseRecents.push();
+                    final DatabaseReference recentsPostPostedBy = recentsPost.child("PostedBy");
 
                     recentsPost.child("infoneContactName").setValue(name);
-                    final DatabaseReference recentsPostPostedBy = recentsPost.child("PostedBy");
                     recentsPostPostedBy.setValue(null);
                     recentsPost.child("infoneContactCategory").setValue(catName);
                     recentsPost.child("id").setValue(key);
@@ -129,8 +135,10 @@ public class InfoneAddContactActivity extends AppCompatActivity {
                         @Override
                         public void onDataChange(DataSnapshot dataSnapshot) {
                             recentsPostPostedBy.child("Username").setValue(dataSnapshot.child("Username").getValue().toString());
+                            newContactNumRef.child("PostedBy").child("Username").setValue(dataSnapshot.child("Username").getValue().toString());
                             //needs to be changed after image thumbnail is put
                             recentsPostPostedBy.child("ImageThumb").setValue(dataSnapshot.child("Image").getValue().toString());
+                            newContactNumRef.child("PostedBy").child("ImageThumb").setValue(dataSnapshot.child("Image").getValue().toString());
                         }
 
                         @Override
