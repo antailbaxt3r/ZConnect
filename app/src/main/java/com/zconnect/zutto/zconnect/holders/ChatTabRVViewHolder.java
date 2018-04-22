59caa2cffb13d267ad4556f2f8e0e3dbc74d7eb3
AkhilKamath <akhilkamath97@gmail.com>
2018-04-22 06:14:18 +0530

    silly billy bug

--- app/src/main/java/com/zconnect/zutto/zconnect/holders/ChatTabRVViewHolder.java
+++ app/src/main/java/com/zconnect/zutto/zconnect/holders/ChatTabRVViewHolder.java
@@ -3,6 +3,7 @@ package com.zconnect.zutto.zconnect.holders;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -16,12 +17,10 @@ public class ChatTabRVViewHolder extends RecyclerView.ViewHolder {
 
     public TextView nametv;
     public LinearLayout linearLayout;
-    public Button del;
 
     public ChatTabRVViewHolder(View itemView) {
         super(itemView);
         nametv = (TextView) itemView.findViewById(R.id.chat_name_message);
-        del=(Button)itemView.findViewById(R.id.delete_btn_chat);
         linearLayout=(LinearLayout) itemView.findViewById(R.id.ll_chat_message);
     }
 }
