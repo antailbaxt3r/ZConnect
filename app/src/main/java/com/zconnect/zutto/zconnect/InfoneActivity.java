6b09252a5721d12c3ea5876594cdc1d2e5eff5dd
AkhilKamath <akhilkamath97@gmail.com>
2018-04-27 03:10:27 +0530

    infone back done

--- app/src/main/java/com/zconnect/zutto/zconnect/InfoneActivity.java
+++ app/src/main/java/com/zconnect/zutto/zconnect/InfoneActivity.java
@@ -46,12 +46,28 @@ public class InfoneActivity extends BaseActivity {
     private SharedPreferences communitySP;
     public String communityReference;
     ProgressBar progressBar;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_infone2);
+        toolbar=(Toolbar) findViewById(R.id.toolbar_app_bar_infone);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+            toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    onBackPressed();
+                }
+            });
+            if (getSupportActionBar() != null)
+                getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+        toolbarSetup();
         setTitle("Infone");
+        toolbar.setTitle("Infone");
         progressBar = (ProgressBar) findViewById(R.id.infone2_progress_circle);
         progressBar.setVisibility(View.VISIBLE);
         recyclerViewCat = (RecyclerView) findViewById(R.id.rv_cat_infone);
@@ -124,6 +140,20 @@ public class InfoneActivity extends BaseActivity {
 
         askCallPermissions();
 
+    }
+
+    private void toolbarSetup() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            int colorPrimary = ContextCompat.getColor(this, R.color.colorPrimary);
+            int colorDarkPrimary = ContextCompat.getColor(this, R.color.colorPrimaryDark);
+            getWindow().setStatusBarColor(colorDarkPrimary);
+            getWindow().setNavigationBarColor(colorPrimary);
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        }
     }
 
     private void addDialog() {
--- app/src/main/java/com/zconnect/zutto/zconnect/NewMessageActivity.java
+++ app/src/main/java/com/zconnect/zutto/zconnect/NewMessageActivity.java
@@ -1,12 +1,15 @@
 package com.zconnect.zutto.zconnect;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Message;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -49,6 +52,28 @@ public class NewMessageActivity extends BaseActivity {
         setContentView(R.layout.activity_new_message);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        setActionBarTitle("Create status");
+        if (toolbar != null) {
+            toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    onBackPressed();
+                }
+            });
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            int colorPrimary = ContextCompat.getColor(this, R.color.colorPrimary);
+            int colorDarkPrimary = ContextCompat.getColor(this, R.color.colorPrimaryDark);
+            getWindow().setStatusBarColor(colorDarkPrimary);
+            getWindow().setNavigationBarColor(colorPrimary);
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        }
 
         mPostedByDetails = FirebaseDatabase.getInstance().getReference().child("communities").child(communityReference).child("Users1").child(FirebaseAuth.getInstance().getCurrentUser().getUid());
 
