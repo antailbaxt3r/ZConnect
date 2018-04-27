46476b09197d47c657897c6125ac59248427152e
G-LOKi <garg.lokesh96@gmail.com>
2018-04-27 10:53:41 +0530

    Messages setting removed

--- app/src/main/java/com/zconnect/zutto/zconnect/MessagesActivity.java
+++ app/src/main/java/com/zconnect/zutto/zconnect/MessagesActivity.java
@@ -70,7 +70,6 @@ public class MessagesActivity extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setHasOptionsMenu(true);
     }
 
     public class SectionsPagerAdapter extends FragmentPagerAdapter {
@@ -124,25 +123,5 @@ public class MessagesActivity extends Fragment {
             return null;
         }
     }
-
-    @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getActivity().getMenuInflater().inflate(R.menu.menu_messages, menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+    
 }
