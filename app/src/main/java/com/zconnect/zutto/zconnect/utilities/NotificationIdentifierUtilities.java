e4dbf437aee3f62ba8e3a3cc0e97ac95edb79a08
G-LOKi <garg.lokesh96@gmail.com>
2018-07-21 00:54:22 +0530

    Notification Settings Bug removed

--- app/src/main/java/com/zconnect/zutto/zconnect/utilities/NotificationIdentifierUtilities.java
+++ app/src/main/java/com/zconnect/zutto/zconnect/utilities/NotificationIdentifierUtilities.java
@@ -2,25 +2,25 @@ package com.zconnect.zutto.zconnect.utilities;
 
 public class NotificationIdentifierUtilities {
 
-    public static final String KEY_NOTIFICATION_EVENT_BOOST = "Event Boosted";
-    public static final String KEY_NOTIFICATION_CAB_JOIN = "Cab Pool Join";
-    public static final String KEY_NOTIFICATION_CAB_LEAVE = "Cab Pool Leave";
-    public static final String KEY_NOTIFICATION_PRODUCT_SHORTLIST = "Product Shortlist";
-    public static final String KEY_NOTIFICATION_INFONE_LIKE = "Infone Like";
-    public static final String KEY_NOTIFICATION_INFONE_LOVE = "Infone Love";
+    public static final String KEY_NOTIFICATION_EVENT_BOOST = "EventBoosted";
+    public static final String KEY_NOTIFICATION_CAB_JOIN = "CabPoolJoin";
+    public static final String KEY_NOTIFICATION_CAB_LEAVE = "CabPoolLeave";
+    public static final String KEY_NOTIFICATION_PRODUCT_SHORTLIST = "ProductShortlist";
+    public static final String KEY_NOTIFICATION_INFONE_LIKE = "InfoneLike";
+    public static final String KEY_NOTIFICATION_INFONE_LOVE = "InfoneLove";
 
-    public static final String KEY_NOTIFICATION_CAB_ADD = "Cab Pool Add";
-    public static final String KEY_NOTIFICATION_PRODUCT_ADD = "Product Add";
-    public static final String KEY_NOTIFICATION_EVENT_ADD = "Event Add";
-    public static final String KEY_NOTIFICATION_FORUM_ADD = "Forum Add";
-    public static final String KEY_NOTIFICATION_INFONE_CATEGORY_ADD = "Infone Add";
-    public static final String KEY_NOTIFICATION_OFFERS_ADD = "Offers Add";
+    public static final String KEY_NOTIFICATION_CAB_ADD = "CabPoolAdd";
+    public static final String KEY_NOTIFICATION_PRODUCT_ADD = "ProductAdd";
+    public static final String KEY_NOTIFICATION_EVENT_ADD = "EventAdd";
+    public static final String KEY_NOTIFICATION_FORUM_ADD = "ForumAdd";
+    public static final String KEY_NOTIFICATION_INFONE_CATEGORY_ADD = "InfoneAdd";
+    public static final String KEY_NOTIFICATION_OFFERS_ADD = "OffersAdd";
 
-    public static final String KEY_NOTIFICATION_CHAT_FORUM = "Forum Chat";
-    public static final String KEY_NOTIFICATION_CHAT_EVENT = "Event Chat";
-    public static final String KEY_NOTIFICATION_CHAT_PRODUCT = "Product Chat";
-    public static final String KEY_NOTIFICATION_CHAT_CAB = "Cab Chat";
-    public static final String KEY_NOTIFICATION_CHAT_POST = "Post Chat";
+    public static final String KEY_NOTIFICATION_CHAT_FORUM = "ForumChat";
+    public static final String KEY_NOTIFICATION_CHAT_EVENT = "EventChat";
+    public static final String KEY_NOTIFICATION_CHAT_PRODUCT = "ProductChat";
+    public static final String KEY_NOTIFICATION_CHAT_CAB = "CabChat";
+    public static final String KEY_NOTIFICATION_CHAT_POST = "PostChat";
 
     public static final String KEY_NOTIFICATION_TEXT_URL = "textURL";
     public static final String KEY_NOTIFICATION_IMAGE_URL = "imageURL";
