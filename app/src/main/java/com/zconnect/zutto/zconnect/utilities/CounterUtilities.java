693c96160778ff7b5ad602efa5fcc03c16e7cb79
G-LOKi <garg.lokesh96@gmail.com>
2018-09-02 21:34:05 +0530

    Counters added (half)

--- app/src/main/java/com/zconnect/zutto/zconnect/utilities/CounterUtilities.java
+++ app/src/main/java/com/zconnect/zutto/zconnect/utilities/CounterUtilities.java
@@ -52,8 +52,51 @@ public class CounterUtilities {
     public static final String KEY_EVENTS_SHARE = "eventsShare";
 
     //storeroom
+    public static final String KEY_STOREROOM_OPEN = "storeroomOpen";
+    public static final String KEY_STOREROOM_PRODUCT_ADD_OPEN = "storeroomProductAddOpen";
+    public static final String KEY_STOREROOM_PRODUCT_ADDED = "storeroomProductAdded";
+    public static final String KEY_STOREROOM_OPEN_PRODUCT = "storeroomOpenProduct";
+    public static final String KEY_STOREROOM_TAB_OPEN = "storeroomCategoryTabOpen";
+    public static final String KEY_STOREROOM_SHORTLIST_OPEN = "storeroomShortlistOpen";
+    public static final String KEY_STOREROOM_MY_PRODUCTS_OPEN = "storeroomMyProductsOpen";
+    public static final String KEY_STOREROOM_OPEN_INDIVIDUAL_CATEGORY = "storeroomIndividualCategory";
+    public static final String KEY_STOREROOM_OPEN_CHAT = "storeroomOpenChat";
+    public static final String KEY_STOREROOM_CALL = "storeroomCall";
+    public static final String KEY_STOREROOM_SHORTLIST = "storeroomShortlist";
+    public static final String KEY_STOREROOM_SHARE = "storeroomShare";
 
-    public static final String KEY_STOREROOM_ = "storeroom";
 
+    //forums
+    public static final String KEY_FORUMS_TAB_OPEN = "forumsTabOpen";
+    public static final String KEY_FORUMS_CATEGORY_OPEN = "forumsCategoryOpen";
+    public static final String KEY_FORUMS_CHANNEL_OPEN = "forumsChannelOpen";
+    public static final String KEY_FORUMS_CREATE_FORUM_OPEN = "forumsCreateForumOpen";
+    public static final String KEY_FORUMS_FORUM_CREATED = "forumsCreated";
+    public static final String KEY_FORUMS_JOINED = "forumsJoined";
+    public static final String KEY_FORUMS_LEAVE = "forumsLeave";
+    public static final String KEY_FORUMS_LIST_MEMBERS = "forumsListMembers";
+    public static final String KEY_FORUMS_EDIT_FORUM = "forumsEditForum";
+
+    //profile
+    public static final String KEY_PROFILE_OPEN = "profileOpen";
+    public static final String KEY_PROFILE_LIKE = "profileLike";
+    public static final String KEY_PROFILE_LOVE  = "profileLove";
+    public static final String KEY_PROFILE_CALL = "profileCall";
+    public static final String KEY_PROFILE_EMAIL = "profileEmail";
+    public static final String KEY_PROFILE_SHARE = "profileShare";
+    public static final String KEY_PROFILE_REPORT = "profileReport";
+
+    //recents
+    public static final String KEY_RECENTS_ADD_STATUS = "recentsAddStatus";
+    public static final String KEY_RECENTS_LIKE = "recentsLike";
+    public static final String KEY_RECENTS_COMMENT = "recentsComment";
+    public static final String KEY_RECENTS_OPEN_IMAGE = "recentsOpenImage";
+    public static final String KEY_RECENTS_OPEN_EVENT = "recentsOpenEvent";
+    public static final String KEY_RECENTS_OPEN_FORUM = "recentsOpenForun";
+    public static final String KEY_RECENTS_OPEN_INFONE_CONTACT = "recentsOpenInfoneContact";
+    public static final String KEY_RECENTS_OPEN_INFONE_CATEGORY = "recentsOpenInfoneCategory";
+    public static final String KEY_RECENTS_OPEN_PRODUCT = "recentsOpenProduct";
+    public static final String KEY_RECENTS_DELETE_POST = "recentsDeletePost";
+    public static final String KEY_RECENTS_SCROLL_TOP = "recentsScrollTop";
 
 }
