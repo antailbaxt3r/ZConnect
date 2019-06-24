e7ec59a7807a870ccd86b4ac182b9ed5215c6353
antailbaxt3r <arjunbajpaiwork@gmail.com>
2019-06-24 16:11:29 +0530

    Changed Admin Notifications

--- app/src/main/java/com/zconnect/zutto/zconnect/NotificationAdmin.java
+++ app/src/main/java/com/zconnect/zutto/zconnect/NotificationAdmin.java
@@ -1,5 +1,6 @@
 package com.zconnect.zutto.zconnect;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
@@ -9,6 +10,7 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.LinearLayout;
 
 import com.zconnect.zutto.zconnect.adapters.NotificationRVAdapter;
 import com.zconnect.zutto.zconnect.commonModules.BaseActivity;
@@ -20,14 +22,16 @@ public class NotificationAdmin extends BaseActivity {
 
     private ArrayList notification_type=new ArrayList<>(Arrays.asList("Image Notification","Text Notification"));
     private Toolbar mActionBarToolbar;
-    private RecyclerView recyclerView;
     private LinearLayoutManager linearLayoutManager;
+    private LinearLayout imageNotification, textNotification;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification_admin);
 
+        imageNotification = findViewById(R.id.image_notification);
+        textNotification = findViewById(R.id.text_notification);
         mActionBarToolbar = (Toolbar) findViewById(R.id.toolbar_app_bar_home);
         setSupportActionBar(mActionBarToolbar);
         getSupportActionBar().setHomeButtonEnabled(true);
@@ -55,10 +59,25 @@ public class NotificationAdmin extends BaseActivity {
 //            getWindow().setNavigationBarColor(colorPrimary);
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         }
-
-        recyclerView=(RecyclerView)findViewById(R.id.menu_notifications_add);
         linearLayoutManager = new LinearLayoutManager(getApplicationContext());
-        recyclerView.setLayoutManager(linearLayoutManager);
-        recyclerView.setAdapter(new NotificationRVAdapter(notification_type,getBaseContext()));
+
+        imageNotification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent nottif_no_image = new Intent(NotificationAdmin.this,NotificationNoImage.class);
+                nottif_no_image.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(nottif_no_image);
+            }
+        });
+
+        textNotification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(NotificationAdmin.this, NotificationNoImage.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+
+            }
+        });
     }
 }
