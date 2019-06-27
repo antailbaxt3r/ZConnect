827b8c5a0b7b4522b512726840d29ea7007257f7
antailbaxt3r <arjunbajpaiwork@gmail.com>
2019-06-28 03:20:31 +0530

    Poll UI

--- app/src/main/java/com/zconnect/zutto/zconnect/AdminHome.java
+++ app/src/main/java/com/zconnect/zutto/zconnect/AdminHome.java
@@ -26,6 +26,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import android.view.WindowManager;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
